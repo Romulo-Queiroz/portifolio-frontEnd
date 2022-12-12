d6ceded98a3f4fcdd8059d6fcc39b1c0d92f36d0
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-12-12 16:23:24 -0300

    build: criando arquivo js para mudar tema do site

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+
+const body = document.querySelector('body')
+
+let btn = document.querySelector('#tema');
+btn.addEventListener('click', changeTema)
+
+function changeTema () {
+    console.log('Cliquei');
+    // body.style.backgroundColor = 'white';
+};
